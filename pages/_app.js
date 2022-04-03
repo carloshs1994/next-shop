804aa7f73829de3e963b0f747f38c78c156897f1
carloshs1994 <carlos.h.s.1994@gmail.com>
2022-04-02 23:43:08 -0600

    Setup Next.js repo and install MUI, edit the _app and _document to use emotion Cache

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import createCache from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideToEmotion = createCache({ key: 'css' })
+function MyApp({ Component, pageProps, emotionCache = clientSideToEmotion }) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Component {...pageProps} />
+    </CacheProvider>
+  )
 }
 
 export default MyApp
